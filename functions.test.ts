d2c1f3a8c528edc60d4f2d4e3175cb9e45b776c6
Alexandr Ketov <ketov.alex.v@gmail.com>
2020-02-12 11:55:04 +0100

    Preparations

---
+++ functions.test.ts
@@ -0,0 +1,9 @@
+import { Functions } from './functions';
+
+describe('Functions Class', () => {
+  const classInstance = new Functions();
+
+  describe('reverse a string', () => {
+    it('should work', () => {});
+  });
+});
---
+++ functions.ts
@@ -0,0 +1,84 @@
+export class Functions {
+
+  /**
+   * Reverses a string.
+   *
+   * @param value String to reverse.
+   * @returns Reversed string.
+   */
+  public ReverseString(value: string): string {
+    throw new Error();
+  }
+
+  /**
+   * Calculates the Nth fibonacci number.
+   *
+   * @param n Fibonacci number to calculate.
+   * @returns The nth fibonacci number.
+   */
+  public CalculateNthFibonacciNumber(n: number): number {
+    throw new Error();
+  }
+
+  /**
+   * Pads a number with up to four zeroes.
+   * Can only pad unsigned numbers up to 99999.
+   *
+   * @param n Number to pad.
+   * @returns Zero-padded number.
+   */
+  public PadNumberWithZeroes(n: number): string {
+    throw new Error();
+  }
+
+  /**
+   * Determines if a year is a leap year.
+   *
+   * @param year Year to determine.
+   * @returns True if leap year, false if not.
+   */
+  public IsLeapYear(year: number): boolean {
+    throw new Error();
+  }
+
+  /**
+   * Find the N:th largest number in a range of numbers.
+   *
+   * @param numbers List of integers.
+   * @param n Largest number to return
+   * @returns The N:th largest number in list.
+   */
+  public FindNthLargestNumber(numbers: number[], n: number): number {
+    throw new Error();
+  }
+
+  /**
+   * Selects the prime numbers from a enumerable with numbers.
+   *
+   * @param numbers Array of numbers.
+   * @returns Array with only prime numbers.
+   */
+  public SelectPrimeNumbers(numbers: number[]): number[] {
+    throw new Error();
+  }
+
+  /**
+   * Determines if the bit pattern of value the same if you reverse it.
+   *
+   * @param value Value to inspect.
+   * @returns True if the bit value is a palindrome otherwise false.
+   */
+  public IsPalindrome(value: number): boolean {
+    throw new Error();
+  }
+
+  /**
+   * Counts all bits in an int value.
+   *
+   * @param value Integer value to count bits in.
+   * @returns Number of bits in integer value.
+   */
+  public CountSetBits(value: number): number {
+    throw new Error();
+  }
+}
